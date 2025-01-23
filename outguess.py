490e70487283d7d000f89aba5ed947902a86e314
Fulgur <fulgurgithub@gmail.com>
2025-01-23 11:17:19 +0100

    Renamed steganography.py to outguess.py

--- outguess.py
+++ outguess.py
@@ -3,20 +3,19 @@ from PIL import Image
 def genData(data):
     newd = []
     for i in data:
-        newd.append(format(ord(i), '08d')) # 8bits
+        newd.append(format(ord(i), '08b')) # 8bits
     return newd
 
 def modPix(pix, data):
     datalist = genData(data)
     lendata = len(datalist)
     imdata = iter(pix)
- 
+
     for i in range(lendata):
         pix = [value for value in imdata.__next__()[:3] + imdata.__next__()[:3] + imdata.__next__()[:3]]
         for j in range(0, 8):
             if (datalist[i][j] == '0' and pix[j]% 2 != 0):
                 pix[j] -= 1
- 
             elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
                 if(pix[j] != 0):
                     pix[j] -= 1
@@ -38,9 +37,7 @@ def modPix(pix, data):
 
 def encode_enc(newimg, data):
     w = newimg.size[0]
-    w = newimg.size[0]
     (x, y) = (0, 0)
- 
     for pixel in modPix(newimg.getdata(), data):
         newimg.putpixel((x, y), pixel)
         if (x == w - 1):
@@ -55,10 +52,8 @@ def encode():
     data = input("Message> ")
     if (len(data) == 0):
         raise ValueError('Data is empty')
-    
     newimg = image.copy()
     encode_enc(newimg, data)
-
     new_img_name = input("Output> ")
     newimg.save(new_img_name, str(new_img_name.split(".")[1].upper()))
 
@@ -67,7 +62,6 @@ def decode():
     image = Image.open(img, 'r')
     data = ''
     imgdata = iter(image.getdata())
- 
     while (True):
         pixels = [value for value in imgdata.__next__()[:3] + imgdata.__next__()[:3] + imgdata.__next__()[:3]]
         binstr = ''
@@ -78,14 +72,16 @@ def decode():
                 binstr += '1'
         data += chr(int(binstr, 2))
         if (pixels[-1] % 2 != 0):
-            return data        
+            return data
+
 def menu():
-    a = int(input(":: Welcome to Steganography ::\n" + "1. Encode\n" + "2. Decode\n" + "Choice> "))
+    a = int(input("== Outguess ==\n1. Encode\n2. Decode\nChoice> "))
     if (a == 1):
         encode()
+ 
     elif (a == 2):
         print("Decoded message: " + decode())
     else:
         raise Exception("Enter correct input")
-
+ 
 menu()
